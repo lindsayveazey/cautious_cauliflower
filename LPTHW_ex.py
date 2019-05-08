9f6475e2bda30f297a480ae48f8d0d7da2ee9649
Lindsay <lindsaymveazey@gmail.com>
2019-05-07 18:37:25 -0700

    Functions and files

--- LPTHW_ex.py
+++ LPTHW_ex.py
@@ -1,23 +1,61 @@
-# ex 18
+'''
+# ex 19
 
-# take multiple arguments
-def print_two(*args):
-    arg1, arg2 = args
-    print(f"arg1: {arg1}, arg2: {arg2}")
+# long fx
 
-# the *args above is useless
-def print_two_again(arg1, arg2):
-    print(f"arg1: {arg1}, arg2: {arg2}")
+def tofu_and_greens(tofu_count, number_of_kale):
+    print(f"You have {tofu_count} types of tofu.")
+    print(f"Luckily, I have {number_of_kale} stalks of kale.")
 
-# this takes a single argument:
-def print_one(arg1):
-    print(f"arg1: {arg1}")
+print("We can give function numbers directly.")
+tofu_and_greens(2, 3)
 
-# and this takes none:
-def no_thanks():
-   print("Hello.")
+print("We can also use variables from the script.")
+amount_of_tofu = 2
+amount_of_kale = 3
 
-print_two("Potato", "Tomato")
-print_two_again("Banana", "Apple")
-print_one("Cookies")
-no_thanks()
+tofu_and_greens(amount_of_tofu, amount_of_kale)
+
+print("We can calculate in the function as well...")
+tofu_and_greens(1+1, 2+1)
+
+print("We can combine variables and math.")
+tofu_and_greens(amount_of_tofu + 50, amount_of_kale + 71)
+'''
+# ex 20
+
+# fx and files
+
+from sys import argv
+
+script, input_file = argv
+# create a fx called print_all
+def print_all(f):
+    print(f.read())
+# f is the file of interest
+def rewind(f):
+    f.seek(0)
+# printing the line count of f
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+# renaming the input file as a variable
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+
+print_all(current_file)
+
+print("Now we rewind.")
+
+rewind(current_file)
+
+print("Let's print 3 lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
